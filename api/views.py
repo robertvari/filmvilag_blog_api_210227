f1d59131a09de2b546f37ececb2ff98ab55bb2e0
Vári Róbert <mail.robertvari@gmail.com>
2021-04-11 18:15:26 +0200

    serializers and views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True, context={"request": request})
+
+        return Response(serializer.data)
