5fc8cce24011306f897bd37140a950c89e980ab6
Vári Róbert <mail.robertvari@gmail.com>
2021-04-11 17:38:31 +0200

    Project init

---
+++ api/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.db.models.signals import post_delete, pre_save
+from django.utils.text import slugify
+from PIL import Image
+import os
+
+
+def image_dir_path(instance, filename):
+    return f"posts/{slugify(instance.title)}/{filename}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to=image_dir_path)
+    summary = models.TextField(max_length=1000)
+    slug = models.SlugField(max_length=200, blank=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        # before save if image replaced?
+        self.replace_image()
+
+        super(Post, self).save(*args, **kwargs)
+
+        # after save resize image
+        self.resize_image()
+
+    def replace_image(self):
+        try:
+            post = Post.objects.get(id=self.id)
+            if post.image.name != self.image.name:
+                post.image.delete(save=False)
+        except:
+            pass
+
+    def resize_image(self):
+        image_path = self.image.path
+        img = Image.open(image_path)
+
+        max_size = 1500
+        if img.size[0] > max_size or img.size[1] > max_size:
+            img.thumbnail((max_size, max_size))
+            img.save(image_path)
+
+    def __str__(self):
+        return self.title
+
+
+def image_cleanup(sender, instance, **kwargs):
+    if os.path.exists(instance.image.path):
+        os.remove(instance.image.path)
+
+
+post_delete.connect(image_cleanup, sender=Post)
+
+
+def slug_generator(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+
+pre_save.connect(slug_generator, sender=Post)
